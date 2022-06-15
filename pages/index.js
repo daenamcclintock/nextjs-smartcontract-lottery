49b085d821132a854412627cfa2cb5d41c45ea6b
daenamcclintock <daena.mcclintock@gmail.com>
2022-06-15 16:20:54 -0400

    Added Lottery Entrance... got abi and contractAddress from back end smart contract

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 // import ManualHeader from '../components/ManualHeader'
-import Header from "../components"
+import Header from "../components/Header"
 import LotteryEntrance from '../components/LotteryEntrance'
 
 export default function Home() {
